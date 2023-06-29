b50d7056b02fe2bdba498151d287f4f67ef93734
Xiannomics <christfontanez@gmail.com>
2023-06-29 07:24:03 -0400

    Working functions for computer and player input

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function getComputerChoice()
+function getComputerChoice() {
 	//random output of rock, paper, or scissors
 	// return this output
-	const choice = ['Rock', 'Paper', 'Scissors'];
-	console.log(choice);
+	const choice = ['Rock', 'Paper', 'Scissors']; // Variable array to contain computer choices
+    const randomChoice = Math.floor(Math.random() * choice.length); // Randomly choose an array item from choice variable
+    const randomChoiceFinal = (randomChoice, choice[randomChoice]); // Picks a rand
+    console.log(randomChoiceFinal);
+}
+// 
+
+function getPlayerChoice() {             //Function to get input from player
+    // input = players choice
+    playerInput = prompt("Enter Rock, Paper, Or Scissors!"); // Variable to get rock 
+    // playerChoiceunused = playerInput.toUpperCase(); // Variable to convert players choice to case insensitive.
+    playerChoice = playerInput[0].toUpperCase() + playerInput.substring(1); // Variable to convert players choice to case insensitive, and capitalized the first letter.
+    // for ((playerChoice != 'Rock') || (playerChoice != 'Paper') || (playerChoice != 'Scissors'))
+    // {
+    //     alert("Enter Rock, Paper, or Scissors!");
+    //     getPlayerChoice();
+    // }
+}
+getPlayerChoice();
+console.log(playerChoice);
+getComputerChoice();
