e3a0c2600dce10bbe1bc0861f656ec14e8a6fcd7
Xiannomics <christfontanez@gmail.com>
2023-06-30 15:27:36 -0400

    Before nested ifs

--- script.js
+++ script.js
@@ -7,49 +7,56 @@ function getComputerChoice() {
     return randomChoiceFinal;
 }
  
+
+function playerSelection() {
+    playerInput = prompt("Enter Rock, Paper, Or Scissors!"); // Variable to get rock
+    playerToLower = playerInput.toLowerCase(); // Converts players input to all lowercase
+    playerSelection = playerToLower[0].toUpperCase() + playerToLower.substring(1); // Uppercases the first letter, then concats it with the rest of the string
+    return playerSelection();
+}
 // playerInput = prompt("Enter Rock, Paper, Or Scissors!"); // Variable to get rock
 // playerChoice = playerInput[0].toUpperCase() + playerInput.substring(1);
 
 //console.log("Computer: " + getComputerChoice());
-
+// Round function needs fixing.
 let lossCounter = 0; // Loss counter for player
 let winCounter = 0; // Win counter for player
-function round(playerSelection, computerSelection) {
-    playerInput = prompt("Enter Rock, Paper, Or Scissors!"); // Variable to get rock
-    playerToLower = playerInput.toLowerCase(); // Converts players input to all lowercase
-    playerSelection = playerToLower[0].toUpperCase() + playerToLower.substring(1); // Uppercases the first letter, then concats it with the rest of the string
+function round() {
+    // playerInput = prompt("Enter Rock, Paper, Or Scissors!"); // Variable to get rock
+    // playerToLower = playerInput.toLowerCase(); // Converts players input to all lowercase
+    // playerSelection() = playerToLower[0].toUpperCase() + playerToLower.substring(1); // Uppercases the first letter, then concats it with the rest of the string
     console.log("Computer: " + getComputerChoice());
-    if (playerSelection === 'Rock' || playerSelection === 'Paper' || playerSelection === 'Scissors') {
-        console.log(playerSelection);
+    if (playerSelection() === 'Rock' || playerSelection() === 'Paper' || playerSelection() === 'Scissors') {
+        console.log(playerSelection());
         // Loss conditions
-            if (playerSelection === 'Rock' && getComputerChoice() === 'Paper') { // Checks if player lost rock outcome
+            if (playerSelection() === 'Rock' && getComputerChoice() === 'Paper') { // Checks if player lost rock outcome
                 console.log("You lose! Paper beats Rock.");
                 lossCounter++;
                 console.log(`You lost ${lossCounter} amount of times.`);
             }
-            else if (playerSelection === 'Paper' && getComputerChoice() === 'Scissors') { // Checks if player lost paper outcome
+            else if (playerSelection() === 'Paper' && getComputerChoice() === 'Scissors') { // Checks if player lost paper outcome
                 console.log("You lose! Scissors beats Paper.");
                 lossCounter++;
                 console.log(`You lost ${lossCounter} amount of times.`);
             }
-            else if (playerSelection === 'Scissors' && getComputerChoice() === 'Rock') {
+            else if (playerSelection() === 'Scissors' && getComputerChoice() === 'Rock') {
                 console.log("You lose! Rock beats Scissors.");
                 lossCounter++;
                 console.log(`You lost ${lossCounter} amount of times.`);
             }
             // console.log(`You lost ${lossCounter} amount of times.`);
             // Win conditions
-            else if (playerSelection === 'Rock' && getComputerChoice() === 'Scissors') {
+            else if (playerSelection() === 'Rock' && getComputerChoice() === 'Scissors') {
                 console.log("You win! Rock beats Scissors.");
                 winCounter++;
                 console.log(`You won ${winCounter} amount of times.`);
             }
-            else if (playerSelection === 'Paper' && getComputerChoice() === 'Rock') {
+            else if (playerSelection() === 'Paper' && getComputerChoice() === 'Rock') {
                 console.log("You win! Paper beats Rock.");
                 winCounter++;
                 console.log(`You won ${winCounter} amount of times.`);
             }
-            else if (playerSelection === 'Scissors' && getComputerChoice() === 'Paper') {
+            else if (playerSelection() === 'Scissors' && getComputerChoice() === 'Paper') {
                 console.log("You win! Scissors beats Paper.");
                 winCounter++;
                 console.log(`You won ${winCounter} amount of times.`);
