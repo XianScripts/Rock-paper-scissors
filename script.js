56373b0959acbc898fbbb598e034277dd26e4c01
Xiannomics <christfontanez@gmail.com>
2023-06-30 19:34:05 -0400

    Finalized a baseline working game with logic for 1 round

--- script.js
+++ script.js
@@ -1,79 +1,69 @@
 function getComputerChoice() {
-	//random output of rock, paper, or scissors
-	// return this output
-	const choice = ['Rock', 'Paper', 'Scissors']; // Variable array to contain computer choices
+    //random output of rock, paper, or scissors
+    // return this output
+    const choice = ['Rock', 'Paper', 'Scissors']; // Variable array to contain computer choices
     const randomChoice = Math.floor(Math.random() * choice.length); // Randomly choose an array item from choice variable
-    const randomChoiceFinal = (randomChoice, choice[randomChoice]); // Picks a random choice
+    const randomChoiceFinal = (randomChoice, choice[randomChoice]); // Picks a random choice 
     return randomChoiceFinal;
 }
- 
+computer = getComputerChoice(); // Assigning a variable to the computer choice function
 
-function playerSelection() {
+function getPlayerChoice() {
     playerInput = prompt("Enter Rock, Paper, Or Scissors!"); // Variable to get rock
     playerToLower = playerInput.toLowerCase(); // Converts players input to all lowercase
     playerSelection = playerToLower[0].toUpperCase() + playerToLower.substring(1); // Uppercases the first letter, then concats it with the rest of the string
-    return playerSelection();
+    return playerSelection;
 }
-// playerInput = prompt("Enter Rock, Paper, Or Scissors!"); // Variable to get rock
-// playerChoice = playerInput[0].toUpperCase() + playerInput.substring(1);
+player = getPlayerChoice(); // Assigning a variable to the player choice function
+compString = "Computer: "; // String to let user know who chose what
+playString = "Player: "; // Same thing as compSring ^
 
-//console.log("Computer: " + getComputerChoice());
-// Round function needs fixing.
-let lossCounter = 0; // Loss counter for player
-let winCounter = 0; // Win counter for player
-function round() {
-    // playerInput = prompt("Enter Rock, Paper, Or Scissors!"); // Variable to get rock
-    // playerToLower = playerInput.toLowerCase(); // Converts players input to all lowercase
-    // playerSelection() = playerToLower[0].toUpperCase() + playerToLower.substring(1); // Uppercases the first letter, then concats it with the rest of the string
-    console.log("Computer: " + getComputerChoice());
-    if (playerSelection() === 'Rock' || playerSelection() === 'Paper' || playerSelection() === 'Scissors') {
-        console.log(playerSelection());
-        // Loss conditions
-            if (playerSelection() === 'Rock' && getComputerChoice() === 'Paper') { // Checks if player lost rock outcome
-                console.log("You lose! Paper beats Rock.");
-                lossCounter++;
-                console.log(`You lost ${lossCounter} amount of times.`);
-            }
-            else if (playerSelection() === 'Paper' && getComputerChoice() === 'Scissors') { // Checks if player lost paper outcome
-                console.log("You lose! Scissors beats Paper.");
-                lossCounter++;
-                console.log(`You lost ${lossCounter} amount of times.`);
-            }
-            else if (playerSelection() === 'Scissors' && getComputerChoice() === 'Rock') {
-                console.log("You lose! Rock beats Scissors.");
-                lossCounter++;
-                console.log(`You lost ${lossCounter} amount of times.`);
-            }
-            // console.log(`You lost ${lossCounter} amount of times.`);
-            // Win conditions
-            else if (playerSelection() === 'Rock' && getComputerChoice() === 'Scissors') {
-                console.log("You win! Rock beats Scissors.");
-                winCounter++;
-                console.log(`You won ${winCounter} amount of times.`);
-            }
-            else if (playerSelection() === 'Paper' && getComputerChoice() === 'Rock') {
-                console.log("You win! Paper beats Rock.");
-                winCounter++;
-                console.log(`You won ${winCounter} amount of times.`);
-            }
-            else if (playerSelection() === 'Scissors' && getComputerChoice() === 'Paper') {
-                console.log("You win! Scissors beats Paper.");
-                winCounter++;
-                console.log(`You won ${winCounter} amount of times.`);
-            }
-            else {
-                console.log("Tie!");
-            }
-
-
-    } 
-    else { // Restarts function if player enters anything but rock,paper,scissors
-        round();
+let winCounter = 0; // Declaring counter for user's number of wins/losses
+let lossCounter = 0;
+let winner; // Declaring winner/loser console.log variables
+let loser;
+let tie = 0;
+function round(computer, player) { // Function to determine winner of a round between user and computer
+    if (player === 'Rock' && computer === 'Paper') { // Checks if player lost rock outcome
+        loser = console.log("You lose! Paper beats Rock.");
+        lossCounter++;
+        return loser;
+    }
+    else if (player === 'Rock' && computer === 'Scissors') { // Checks if player won rock outcome
+        winner = console.log("You win! Rock beats Scissors.");
+        winCounter++;
+        return winner;
+    }
+    else if (player === 'Paper' && computer === 'Scissors') { // Checks if player lost paper outcome
+        loser = console.log("You lose! Scissors beats Paper.");
+        lossCounter++;
+        return loser;
+    }
+    else if (player === 'Paper' && computer === 'Rock') { // Checks if player won paper outcome
+        winner = console.log("You win! Paper beats Rock.");
+        winCounter++;
+        return winner;
+    }
+    else if (player === 'Scissors' && computer === 'Rock') { // Checks if player lost scissors outcome
+        loser = console.log("You lose! Rock beats Scissors");
+        lossCounter++;
+        return loser;
+    }
+    else if (player === 'Scissors' && computer === 'Paper') { // Checks if player won scissors outcome
+        winner = console.log("You win! Scissors beats Paper.");
+        winCounter++;
+        return winner;
+    }
+    else {
+        console.log("Tie!");	
+        
     }
 }
+// Figure out what to do if user types something other than Rock, paper, scissors
 
-round();
-
+console.log(compString + computer); // Prints our computer and compString so user can see who put what
+console.log(playString + player); // Same as above but for player ^
+round(computer, player);
 
 
 
@@ -123,4 +113,4 @@ if playerChoice === Rock && getComputerChoice() === 'Paper
     //     playerInput = prompt("Enter Rock, Paper, Or Scissors!");
     // }
 //     return playerChoice;
-// }
+// 
