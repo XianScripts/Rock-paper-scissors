1f43d151582f10f5acebcb18f8a2ebb21a4a8737
Xiannomics <christfontanez@gmail.com>
2023-08-10 23:09:34 -0400

    Cleaned up code

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-compString = "Computer: "; // String to let user know who chose what
-playString = "Player: "; // Same thing as compSring ^
-
 // Game logic segment
-
 let winCounter = 0; // Declaring counter for user's number of wins/losses
 let lossCounter = 0;
 let winner; // Declaring winner/loser console.log variables
@@ -20,68 +16,39 @@ function getComputerChoice() {
     return compChoice;
 }
 function round(computer, player) { // Function to determine winner of a round between user and computer
-    
-
     if (player === 'Rock' && computer === 'Paper') { // Checks if player lost rock outcome
-        console.log(compString + 'Paper');
-        console.log(playString + 'Rock');
-        loser = console.log("You lose! Paper beats Rock.");
         lossCounter++;
         return loser;
     }
     else if (player === 'Rock' && computer === 'Scissors') { // Checks if player won rock outcome
-        console.log(compString + 'Scissors');
-        console.log(playString + 'Rock');
-        winner = console.log("You win! Rock beats Scissors.");
         winCounter++;
         return winner;
     }
     else if (player === 'Rock' && computer === 'Rock') { // Checks for tie
-        console.log(compString + 'Rock');
-        console.log(playString + 'Rock');
-        tie = console.log("Tie!");
         tieCounter++;
         return tie;
     }
     else if (player === 'Paper' && computer === 'Scissors') { // Checks if player lost paper outcome
-        console.log(compString + 'Scissors');
-        console.log(playString + 'Paper');
-        loser = console.log("You lose! Scissors beats Paper.");
         lossCounter++;
         return loser;
     }
     else if (player === 'Paper' && computer === 'Rock') { // Checks if player won paper outcome
-        console.log(compString + 'Rock');
-        console.log(playString + 'Paper');
-        winner = console.log("You win! Paper beats Rock.");
         winCounter++;
         return winner;
     }
     else if (player === 'Paper' && computer === 'Paper') { // Checks for tie
-        console.log(compString + 'Paper');
-        console.log(playString + 'Paper');
-        tie = console.log("Tie!");
         tieCounter++;
         return tie;
     }
     else if (player === 'Scissors' && computer === 'Rock') { // Checks if player lost scissors outcome
-        console.log(compString + 'Rock');
-        console.log(playString + 'Scissors');
-        loser = console.log("You lose! Rock beats Scissors");
         lossCounter++;
         return loser;
     }
     else if (player === 'Scissors' && computer === 'Paper') { // Checks if player won scissors outcome
-        console.log(compString + 'Paper');
-        console.log(playString + 'Scissors');
-        winner = console.log("You win! Scissors beats Paper.");
         winCounter++;
         return winner;
     }
     else if (player === 'Scissors' && computer === 'Scissors') { // Checks for tie
-        console.log(compString + 'Scissors');
-        console.log(playString + 'Scissors');
-        tie = console.log("Tie!");
         tieCounter++;
         return tie;
     }
@@ -89,14 +56,10 @@ function round(computer, player) { // Function to determine winner of a round be
         console.log("Refresh and try again, please only enter 'Rock', 'Paper', 'Scissors'");
         
     }
-    
-       
-    
 }
 
 
 // UI segment
-
 let resultText = document.querySelector('#game-results').children;
 let resultContainer = document.querySelector('#game-results');
 const rock = document.querySelector('#rock');
@@ -127,7 +90,7 @@ scissors.addEventListener('click', () => {
 })
 
 
-
+// Function to determine round and winner
 function playcounter() {
     console.log("Wins: " + winCounter);
     console.log("Losses: " + lossCounter);
@@ -162,8 +125,6 @@ function playcounter() {
         console.log(newButton.outerHTML);
         console.log(newButtonContainer.outerHTML);
         newButtonContainer.appendChild(newButton);
-        // Continue button 
-        // newButtonContainer.appendChild(continueButton);
         newButton.addEventListener('click', () => {
             playCount = 0;
             winCounter = 0;
@@ -189,10 +150,10 @@ function playcounter() {
 
 
     }
-
+    // Output text of current round info
     resultText[0].textContent = `Computer: ${computer}`;
     resultText[1].textContent = `Wins so far: ${winCounter}`;
     resultText[2].textContent = `Losses so far: ${lossCounter}`;
     resultText[3].textContent = `Ties so far: ${tieCounter}`;
-    // textResults.textContent = `Losses so far: ${lossCounter}`;
+
 }
